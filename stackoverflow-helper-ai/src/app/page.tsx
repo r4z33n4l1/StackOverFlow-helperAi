ae7432cffd7638a8d0a93a89011a7eeb5a56fc9c
Razeen <razeen_ali@yahoo.com>
2023-06-24 19:51:04 -0400

    integrating all functions, but facing api call errors, so working that right now

--- stackoverflow-helper-ai/src/app/page.tsx
+++ stackoverflow-helper-ai/src/app/page.tsx
@@ -4,25 +4,45 @@ import aiFetch from './utils/api';
 import { linkValid, linkFetch } from './utils/linktools';
 
 export default function Home() {
-
   const [link, setLink] = useState('');
   const [question, setQuestion] = useState('');
   const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState('');
 
   const handleClick = async () => {
     const response = await aiFetch('Hello bestie!');
     console.log(response);
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!linkValid(link)) {
+      console.log('Invalid link');
+      return;
+    }
+
+    setLoading(true);
+    const bodyContent = await linkFetch(link);
+    setLoading(false);
+
+    const usePrompt = `My question is ${question}. Give me the answer from: ${bodyContent}`;
+    console.log(usePrompt);
+    const aiResponse = await aiFetch(usePrompt);
+    setResponse(aiResponse);
+  };
+
   return (
     <div>
       <h1>Hello guys! This is the start!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Link" value={link} onChange={(e) => setLink(e.target.value)} />
         <br />
         <input type="text" placeholder="Question" value={question} onChange={(e) => setQuestion(e.target.value)} />
-      <button onClick={handleClick}>Click me!</button>
+        <button type="submit">Submit</button>
       </form>
+      {loading && <p>Loading...</p>}
+      {response && <p>{response}</p>}
     </div>
   );
 }
--- stackoverflow-helper-ai/src/app/utils/api.tsx
+++ stackoverflow-helper-ai/src/app/utils/api.tsx
@@ -6,12 +6,15 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const defaultPrompt = { role: 'system', content: 'Just say hello' }
+const defaultPrompt = { role: 'system', content: 'You are an amazing stackoverflow answerer, you shall analyze the prompt from the user and give a summary of their question based on the body of the text' }
 
 
 export default async function aiFetch(prompt: string) {
+    
+  console.log(prompt);
+
     const messages = [defaultPrompt, { role: 'user', content: prompt }];
-  
+
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages,
