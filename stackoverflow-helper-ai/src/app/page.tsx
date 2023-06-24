4b6a1b0c7406b356ef8a3b1cd44377c7b1210f6f
Razeen <razeen_ali@yahoo.com>
2023-06-24 19:32:39 -0400

    did slight styling changes, got the api call to work, started working on link parser

--- stackoverflow-helper-ai/src/app/page.tsx
+++ stackoverflow-helper-ai/src/app/page.tsx
@@ -1,18 +1,26 @@
 'use client'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import aiFetch from './utils/api';
 
 export default function Home() {
-  const [response, setResponse] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const apiResponse = await aiFetch('Hello bestie!');
-      setResponse(apiResponse);
-    };
+  const [link, setLink] = useState('');
+  const [question, setQuestion] = useState('');
 
-    fetchData();
-  }, []);
+  const handleClick = async () => {
+    const response = await aiFetch('Hello bestie!');
+    console.log(response);
+  };
 
-  return <h1>{response}</h1>;
+  return (
+    <div>
+      <h1>Hello guys! This is the start!</h1>
+      <form>
+        <input type="text" placeholder="Link" value={link} onChange={(e) => setLink(e.target.value)} />
+        <br />
+        <input type="text" placeholder="Question" value={question} onChange={(e) => setQuestion(e.target.value)} />
+      <button onClick={handleClick}>Click me!</button>
+      </form>
+    </div>
+  );
 }
